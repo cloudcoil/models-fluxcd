9c9c24f0a9aa6d56655a14096afd660cb410e26e
Sambhav Kothari <sambhavs.email@gmail.com>
2026-09-07 12:35:55 +0100

    Update Flux to 2.9.5 and Cloudcoil 0.7 (#1)
    * Update Flux to 2.9.5 and Cloudcoil 0.7
    
    * Require released Cloudcoil 0.7.1

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,3 +6,10 @@ import cloudcoil.models.fluxcd as fluxcd
 def test_has_modules():
     modules = list(filter(lambda x: isinstance(x, ModuleType), fluxcd.__dict__.values()))
     assert modules, "No modules found in fluxcd"
+
+
+def test_resource_identity():
+    from cloudcoil.models.fluxcd.source.v1 import GitRepository
+
+    assert GitRepository.gvk().api_version == "source.toolkit.fluxcd.io/v1"
+    assert GitRepository.gvk().kind == "GitRepository"
